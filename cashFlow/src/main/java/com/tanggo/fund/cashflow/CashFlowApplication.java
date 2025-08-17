25f14c2dfe688dc6fbbcccd967fb9c3254d28027
thy3368 <thy_email@126.com>
2025-08-18 01:08:48 +0800

    update

--- cashFlow/src/main/java/com/tanggo/fund/cashflow/CashFlowApplication.java
+++ cashFlow/src/main/java/com/tanggo/fund/cashflow/CashFlowApplication.java
@@ -1,13 +1,21 @@
 package com.tanggo.fund.cashflow;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * 全球资金流动监控系统主应用
+ */
 @SpringBootApplication
+@EnableScheduling
+@Slf4j
 public class CashFlowApplication {
-
+    
     public static void main(String[] args) {
+        log.info("启动全球资金流动监控系统...");
         SpringApplication.run(CashFlowApplication.class, args);
+        log.info("全球资金流动监控系统启动完成");
     }
-
 }
